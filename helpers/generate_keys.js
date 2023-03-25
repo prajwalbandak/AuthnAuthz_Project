a3e656f071845cee31ac9dafbe9dbbbc502f3c1a
prajwalbandak <62823252+prajwalbandak@users.noreply.github.com>
2023-03-25 21:42:26 +0530

    Update generate_keys.js

--- helpers/generate_keys.js
+++ helpers/generate_keys.js
@@ -1,5 +1,6 @@
+
 const crypto = require('crypto')
 const key1 = crypto.randomBytes(32).toString('hex')
 const key2 = crypto.randomBytes(32).toString('hex')
 
-console.table({ key1 , key2 })
+console.table({ key1,key2 })
